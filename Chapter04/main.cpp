4446ed4fe1dd2a29922e9470fee5cca41c659b51
Norman Parker <NParker@avidxchange.com>
2023-10-26 07:03:30 -0600

    cin, ? operator

--- Chapter04/main.cpp
+++ Chapter04/main.cpp
@@ -13,4 +13,17 @@ int main() {
         cout << "    \'" << c << "\':" << int(c) << ((c + 1) % 8 ? ' ' : '\n');
     }
     cout << '\n';
+    cout << " -------------------------- " << endl;
+    string s1 = "Good";
+    string s2 = "Morning";
+    cout << s1 << " " << s2 << " strings." << endl;
+    cout << " -------------------------- " << endl;
+    first = 29;
+    second = 4;
+    int result = (first > second) ? first : second;
+    cout << "The bigger number of " << first << " and " << second << " is " << result << endl;
+    cout << " -------------------------- " << endl;
+    cout << "Type your favorite number:";
+    cin >> first;
+    cout << "Your favorite number is " << first << endl;
 }
