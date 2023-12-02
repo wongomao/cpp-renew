296d2d3e8a50f254001fb930ea1795299eb9ef4e
Norman Parker <NParker@avidxchange.com>
2023-12-02 07:43:13 -0700

    showing distribution of random floats

--- book_learningCpp/ch05/main03.cpp
+++ book_learningCpp/ch05/main03.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <random>
 
 void display_array(float *arr, int size)
@@ -24,7 +25,28 @@ void display_grid(float *grid, int size) // square grid
 
 void add_to_bucket(unsigned int *bucket, int size, float value)
 {
-    // LEFT OFF HERE
+    for (int i=0; i<size; ++i) {
+        if (value < (i+1)) {
+            bucket[i] += 1;
+            break;
+        }
+    }
+}
+
+void display_bucket(unsigned int *bucket, int size, int iterations)
+{
+    std::cout << "-----------bucket:----" << std::endl;
+    for (int i=0; i<size; ++i) {
+        std::cout << std::setw (10) << bucket[i] << " ";
+        double ratio = (double)bucket[i] / (double)iterations;
+        int ratio_int = (int)(ratio * 1000.0);
+        // std::cout << " (" << ratio_int << "%) ";
+        for (int j=0; j<ratio_int; ++j) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "----------------------" << std::endl;
 }
 
 int main()
@@ -50,15 +72,27 @@ int main()
     display_grid(*barr, size);
 
     unsigned int bucket[10]{};
-    for (int i=0; i<1000; ++i) {
-        float val = dis(seed);
-        if (val < 1.0) {
-            bucket[0] += 1;
-        }
-        else if (val < 2.0) {
-            bucket[1] += 1;
-        }
+    int iterations = 100000;
+    for (int i=0; i<iterations; ++i) {
+        float val = dis(seed) * 10.0;
+        add_to_bucket(bucket, 10, val);
     }
+    display_bucket(bucket, 10, iterations);
 
+/*
+The output typically shows a uniform distribution of values in the bucket array.
+-----------bucket:----
+      9895 **************************************************************************************************
+     10051 ****************************************************************************************************
+      9994 ***************************************************************************************************
+     10016 ****************************************************************************************************
+      9819 **************************************************************************************************
+      9918 ***************************************************************************************************
+     10119 *****************************************************************************************************
+      9963 ***************************************************************************************************
+     10146 *****************************************************************************************************
+     10079 ****************************************************************************************************
+----------------------
+*/
     return 0;
 }
